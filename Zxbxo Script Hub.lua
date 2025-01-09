b99a16a3545b99bf2ff835506b0d9a3460981e11
Mc4121ban <105984396+Mc4121ban@users.noreply.github.com>
2025-01-09 15:24:45 +0700

    Update Zxbxo Script Hub.lua

--- Zxbxo Script Hub.lua
+++ Zxbxo Script Hub.lua
@@ -1,72 +1,7 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Mc4121ban/ZxBxo-Script/refs/heads/main/New%20Ui.lua"))()
---[[
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local ui = "New" --// Old
-function run(ui)
 pcall(function()
-    OrionLib:Destroy()
+    local dhmap = {2788229376, 7213786345, 16033173781}
+    if table.find(dhmap, game.PlaceId) then
+        game.Players.LocalPlayer:Kick("stop using this on dh this not have ac bypass.")
+    end
 end)
-    if ui == "New" then
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/Mc4121ban/ZxBxo-Script/refs/heads/main/New%20Ui.lua"))()
-    else
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/Mc4121ban/ZxBxo-Script/refs/heads/main/Old%20Ui.lua"))()
-    end
-end
-local filename = "whynot.txt"
-local function read()
-    local success, data = pcall(function()
-        return readfile(filename)
-    end)
-    if success then
-        return data
-    else
-        return nil
-    end
-end
-local function delete()
-    local success, message = pcall(function()
-        delfile(filename)
-    end)
-end
-local function save(content)
-    local success, errorMsg = pcall(function()
-        writefile(filename, content)
-    end)
-end
-local a = read()
-if a then
-run(a)
-return
-end
-local Window = OrionLib:MakeWindow({Name = "Ui Select", HidePremium = false, IntroEnabled = false, SaveConfig = true, ConfigFolder = "OrionTest"})
-local Main = Window:MakeTab({
-	Name = "Main",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
-})
-Main:AddDropdown({
-	Name = "Ui",
-	Default = "New",
-	Options = {"New", "Old"},
-	Callback = function(Value)
-		ui = Value
-	end    
-})
-Main:AddButton({
-	Name = "Loaded",
-	Callback = function()
-      		run(ui)
-  	end    
-})
-Main:AddToggle({
-	Name = "Save",
-	Default = false,
-	Callback = function(Value)
-	    if Value then
-		save(ui)
-	    else
-	         delete()
-	    end
-	end    
-})
-]]
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Mc4121ban/ZxBxo-Script/refs/heads/main/New%20Ui.lua"))()
