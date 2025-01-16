be47a70397024b0faf3121259642fdd705a93317
Mc4121ban <105984396+Mc4121ban@users.noreply.github.com>
2025-01-16 22:57:42 +0700

    Update Zxbxo Script Hub.lua

--- Zxbxo Script Hub.lua
+++ Zxbxo Script Hub.lua
@@ -1,7 +1,7 @@
 pcall(function()
     local dhmap = {2788229376, 7213786345, 16033173781}
     if table.find(dhmap, game.PlaceId) then
-        game.Players.LocalPlayer:Kick("stop using this on dh this not have ac bypass.")
+        warn("stop using this on dh this not have ac bypass.")
     end
 end)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Mc4121ban/ZxBxo-Script/refs/heads/main/New%20Ui.lua"))()
